2afc8933a1e2794bcf782820fb564cf099736c7a
Ryan Morrison <ryanmorrison@gmail.com>
2025-02-27 09:27:31 +0000

    change name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 flex items-center">
-            <h1 className="text-xl font-bold text-gray-900">Transcriber</h1>
+            <h1 className="text-xl font-bold text-gray-900">Transcriber5</h1>
           </div>
           <div>
             <span className="text-gray-600 text-sm font-medium">
